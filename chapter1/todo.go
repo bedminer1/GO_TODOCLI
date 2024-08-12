bd4b3640bcc709f4973159d44461c768b4818fec
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-12 20:26:45 +0800

    added Add and Complete methods for List

--- chapter1/todo.go
+++ chapter1/todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // item is a TODO item
 type item struct {
@@ -12,3 +15,28 @@ type item struct {
 
 // List represents a list of TODO items
 type List []item
+
+// Add creates a new todo item n appends to List
+func (l *List) Add(task string) {
+	t := item {
+		Task: task,
+		Done: false,
+		CreatedAt: time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+// Complete marks a TODO item as complete
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
