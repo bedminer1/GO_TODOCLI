2f59c21c3a787614b5c336862504e9a644a3cd3a
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-19 22:21:05 +0800

    TestTodoCLI to run tests for adding and listing tasks

--- no_cobra/cmd/todo/main_test.go
+++ no_cobra/cmd/todo/main_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -20,8 +22,9 @@ func TestMain(m *testing.M) {
 		binName += ".exe"
 	}
 
-	build := exec.Command("go", "build", "-o", binName)
+	build := exec.Command("go", "build", "-o", binName) 
 
+	// create executable binary
 	if err := build.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
 		os.Exit(1)
@@ -36,3 +39,33 @@ func TestMain(m *testing.M) {
 	os.Exit(result)
 }
 
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := task + "\n"
+		if expected != string(out) {
+			t.Errorf("Expect %q, got %q instead\n", expected, string(out))
+		}
+	})
+}
