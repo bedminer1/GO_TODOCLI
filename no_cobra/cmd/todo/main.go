4b5ae7209aec0c496a38e57df2d7c1fbeb7b3819
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-19 20:39:29 +0800

    Finished writing basic switch statement to handle displaying all tasks and adding tasks

--- no_cobra/cmd/todo/main.go
+++ no_cobra/cmd/todo/main.go
@@ -1,5 +1,38 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	todo "github.com/bedminer1/chapter1todo"
+)
+
+// Hardcode file name
+const todoFileName = ".todo.json"
 
 func main() {
-	
+	l := &todo.List{}
+
+	// Use Get method to read from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on num of arguments
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 }
