6f47d1af9ed56110d058683f84f2410431c1bc11
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-20 21:14:09 +0800

    updated test to also test for input from Stdin instead of just command-line arguments

--- no_cobra/cmd/todo/main_test.go
+++ no_cobra/cmd/todo/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -48,8 +49,23 @@ func TestTodoCLI(t *testing.T) {
 
 	cmdPath := filepath.Join(dir, binName)
 
-	t.Run("AddNewTask", func(t *testing.T) {
+	t.Run("AddNewTaskFromArguments", func(t *testing.T) {
 		cmd := exec.Command(cmdPath, "-add", task) // adds the task
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	task2 := "test task number 2"
+	t.Run("AddNewTaskFromStdIn", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
@@ -62,24 +78,7 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatal(err)
 		}
 		
-		expected := fmt.Sprintf("   1: %s\n", task)
-		if expected != string(out) {
-			t.Errorf("Expect %q, got %q instead\n", expected, string(out))
-		}
-	})
-	
-	t.Run("CompleteTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, "-complete", "1")
-		if err := cmd.Run(); err != nil {
-			t.Fatal(err)
-		}
-
-		cmd = exec.Command(cmdPath, "-list")
-		out, err := cmd.CombinedOutput() // run cmd and get uncompleted tasks in stdout
-		if err != nil {
-			t.Fatal(err)
-		}
-		expected := fmt.Sprintf("X  1: %s\n", task)
+		expected := fmt.Sprintf("   1: %s\n   2: %s\n", task, task2)
 		if expected != string(out) {
 			t.Errorf("Expect %q, got %q instead\n", expected, string(out))
 		}
