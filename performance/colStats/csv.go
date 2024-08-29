a56a114f8d50dbd1c9f2ad469d82733be91e8d24
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-29 14:27:30 +0800

    optimized memory allocation by using read instead of readall, only need memory to store one row instead of whole file

--- performance/colStats/csv.go
+++ performance/colStats/csv.go
@@ -23,19 +23,25 @@ func avg(data []float64) float64 {
 
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 	column-- // assume 1-indexing
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
 
 	var data []float64
-	for i, row := range allData {
-		if i == 0 {
+	for i := 0; ;i++ {
+		row, err := cr.Read()
+		if err == io.EOF { // variable limit of loop
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
+		if i == 0 { // skip header
 			continue
 		}
+
 		if len(row) <= column {
-			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+			return nil, fmt.Errorf("%w: File only has %d columns", ErrInvalidColumn, len(row))
 		}
 
 		v, err := strconv.ParseFloat(row[column], 64)
