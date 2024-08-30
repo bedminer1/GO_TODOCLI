72f3039c775c25ab828fef3466480627754faa14
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-30 14:18:36 +0800

    main and run function for goci written

--- processes/goci/main.go
+++ processes/goci/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	proj := flag.String("p", "", "Project directory")
+	flag.Parse()
+
+	if err := run(*proj, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(proj string, out io.Writer) error {
+	if proj == "" {
+		return fmt.Errorf("project directory is required")
+	}
+
+	args := []string{"build", ".", "errors"} // avoid creating file so no cleanup
+	cmd := exec.Command("go", args...)
+	cmd.Dir = proj
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("'go build' failed: %s", err)
+	}
+	_, err := fmt.Fprintln(out, "Go build: SUCCESS")
+	return err
+}
