684b58b3db14c4b08a12d0312b5c96ab63230a17
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-01 16:01:13 +0800

    code for scanning a set of ports on a list of hosts

--- cobra/pScan/cmd/hosts.go
+++ cobra/pScan/cmd/hosts.go
@@ -1,12 +1,9 @@
 /*
 Copyright © 2024 bedminer1
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -17,13 +14,8 @@ var hostsCmd = &cobra.Command{
 	Long: `Add hosts with the add command
 Delete hosts with the delete command
 List hosts with the list command`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hosts called")
-	},
 }
 
 func init() {
 	rootCmd.AddCommand(hostsCmd)
-
-	
 }
