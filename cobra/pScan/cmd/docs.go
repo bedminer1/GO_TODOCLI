aae99a41012a7e711e95fe09ace96a0711bc1a70
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-02 01:01:47 +0800

    docs command to generate docs, generated docs

--- cobra/pScan/cmd/docs.go
+++ cobra/pScan/cmd/docs.go
@@ -1,40 +1,49 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
 // docsCmd represents the docs command
 var docsCmd = &cobra.Command{
 	Use:   "docs",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Generating documentation for your command",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("docs called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir, err := cmd.Flags().GetString("dir")
+		if err != nil {
+			return err
+		}
+
+		if dir == "" {
+			if dir, err = os.MkdirTemp("", "pScan"); err != nil {
+				return err
+			}
+		}
+
+		return docsAction(os.Stdout, dir)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(docsCmd)
 
-	// Here you will define your flags and configuration settings.
+	docsCmd.Flags().StringP("dir", "d", "", "Destination directory for docs")
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// docsCmd.PersistentFlags().String("foo", "", "A help for foo")
+func docsAction(out io.Writer, dir string) error {
+	if err := doc.GenMarkdownTree(rootCmd, dir); err != nil {
+		return err
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// docsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	_, err := fmt.Fprintf(out, "Documentation successfully created in %s\n", dir)
+	return err
 }
