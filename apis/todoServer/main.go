fb633fa6635f0665e246e6c92ad52d9332c9cb7c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-04 22:35:24 +0800

    im gonna go crazy with this folder shit

---
+++ apis/todoServer/main.go
@@ -0,0 +1,10 @@
+package main
+
+// import "flag"
+
+func main() {
+	// host := flag.String("h", "localhost", "Server host")
+	// port := flag.Int("p", 8080, "Server port")
+	// todoFile := flag.String("f","todoServer.json", "todo JSON file")
+	// flag.Parse()
+}
