d2cb1e2ad742b1ebdcc978a513fd161fe05e9f83
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-24 15:17:32 +0800

    Wrote summary logic and widgets

--- pomoSQL/pomo/pomodoro/repository/inMemory.go
+++ pomoSQL/pomo/pomodoro/repository/inMemory.go
@@ -1,10 +1,13 @@
+//go:build inmemory
 // +build inmemory
 
 package repository
 
 import (
 	"fmt"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/bedminer1/pomo/pomodoro"
 )
@@ -74,7 +77,7 @@ func (r *inMemoryRepo) Breaks(n int) ([]pomodoro.Interval, error) {
 	defer r.RUnlock()
 
 	data := []pomodoro.Interval{}
-	for k := len(r.intervals)-1; k>=0; k-- {
+	for k := len(r.intervals) - 1; k >= 0; k-- {
 		if r.intervals[k].Category == pomodoro.CategoryPomodoro {
 			continue
 		}
@@ -86,4 +89,22 @@ func (r *inMemoryRepo) Breaks(n int) ([]pomodoro.Interval, error) {
 	}
 
 	return data, nil
+}
+
+func (r *inMemoryRepo) CategorySummary(day time.Time, filter string) (time.Duration, error) {
+	r.RLock
+	defer r.RUnlock()
+
+	var d time.Duration
+	filter = strings.Trim(filter, "%")
+
+	for _, i := range r.intervals {
+		if i.StartTime.Year() == day.Year() && i.StartTime.YearDay() == day.YearDay() {
+			if strings.Contains(i.Category, filter) {
+				d += i.ActualDuration
+			}
+		}
+	}
+
+	return d, nil
 }
